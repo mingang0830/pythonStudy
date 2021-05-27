ae6e3a9867fa7c0eb957052ac13e33a88318482e
mingyeong <mingang0830@gmail.com>
2021-05-27 11:04:03 +0900

    고친 계산기

--- calculator.py
+++ calculator.py
@@ -1,42 +1,43 @@
 print("=======계산기=======")
+
+ERROR_MESSAGE = "잘못된 입력입니다."
+
+
+def is_digit_with_input(message):
+    character = input("{} 수 입력 : ".format(message))
+    return character, character.isdigit()
+
+
+def validate_digit(is_digit):
+    if is_digit:
+        return True
+
+    print(ERROR_MESSAGE)
+    return False
+
+
 while True:
-    a = input("첫번째 수 입력 : ")
-    if a.isdigit():
-        b = input("연산 입력 : ")
-        if b == "+":
-            c = input("두번째 수 입력 : ")
-            if c.isdigit():
-                print(float(a)+float(c))
-                break
-            else:
-                print("잘못된 입력입니다.")
-                continue
-        elif b == "-":
-            c = input("두번째 수 입력 : ")
-            if c.isdigit():
-                print(float(a)-float(c))
-                break
-            else:
-                print("잘못된 입력입니다.")
-                continue
-        elif b == "*":
-            c = input("두번째 수 입력 : ")
-            if c.isdigit():
-                print(float(a)*float(c))
-                break
-            else:
-                print("잘못된 입력입니다.")
-                continue
-        elif b == "/":
-            c = input("두번째 수 입력 : ")
-            if c.isdigit():
-                print(float(a)/float(c))
-                break
-            else:
-                print("잘못된 입력입니다.")
-                continue
-        else:
-            print("잘못된 입력입니다.")
-            continue
-    else:
-        print("잘못된 입력입니다.")
+    first_digit, is_digit = is_digit_with_input("첫번째")
+    if not validate_digit(is_digit):
+        continue
+
+    operator = input("연산 입력 : ")
+    if operator not in ["+", "-", "*", "/"]:
+        print(ERROR_MESSAGE)
+        continue
+
+    second_digit, is_digit = is_digit_with_input("두번째")
+    if not validate_digit(is_digit):
+        continue
+
+    result = None
+    if operator == "+":
+        result = float(first_digit) + float(second_digit)
+    elif operator == "-":
+        result = float(first_digit) - float(second_digit)
+    elif operator == "*":
+        result = float(first_digit) * float(second_digit)
+    elif operator == "/":
+        result = float(first_digit) / float(second_digit)
+
+    print(result)
