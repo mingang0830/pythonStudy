e95013710fac3e46a95f5dcb8230d8dfad0ac8a2
mingyeong <mingang0830@gmail.com>
2021-10-07 12:47:33 +0900

    아이돌 서비스
    공연할 수 있습니다. 공연하면 공연 횟수를 올립니다.
    만약 팬클럽이 있다면
    공연할때마다 팬클럽에 속한 사람들의 10%(반올림)만큼 팬이 증가합니다.
    없다면
    30% 확률로 팬클럽이 생깁니다.

--- idol_system.py
+++ idol_system.py
@@ -1,15 +1,42 @@
-class People:
-    def __init__(self, name):
+from random import choice
+
+
+class Fanclub:
+    def __init__(self, fanclub):
+        self.total = 0  # 팬클럽 회원 수
+        self.fanclub = fanclub  # 팬클럽 이름
+
+    def join(self, number):
+        self.total = self.total + number
+
+    def withdraw(self, number):
+        self.total = self.total - number
+
+
+class People(Fanclub):
+    def __init__(self, name, fanclub):
+        super().__init__(fanclub)
         self.name = name
         self.perform = 0  # 공연 횟수
 
+    def is_fanclub(self):
+        # 팬클럽이 있으면 True 없으면 False 리턴
+        if self.fanclub is not None:
+            return True
+        else:
+            return False
+
     def performance(self):
         self.perform += 1
-        pass
-
-    def is_fanclub(self):
-
-        pass
+        # 만약 팬클럽이 있다면
+        if self.is_fanclub():
+            # 공연할때마다 팬클럽에 속한 사람들의 10%(반올림)만큼 팬이 증가합니다.
+            self.total += int(self.total * 0.1)
+        else:
+            # 30% 확률로 팬클럽 생성
+            if choice([1,1,1,2,2,2,2,2,2]) == 1:
+                self.fanclub = input('팬클럽 생성 : ')  # 팬클럽 이름 지정
+                self.total = 10  # 팬클럽 회원수 추가
 
     def audition(self):
         pass
@@ -31,19 +58,6 @@ class Group(Idol):  # idol에서 데려오기
         pass
 
 
-class Fanclub(Group):
-    def __init__(self, i_name, g_name, fanclub, name):
-        super().__init__(i_name, g_name, fanclub, name)
-        self.total = None  # 팬클럽 회원 수
-
-    def join(self, number):
-        self.total = self.total + number
-
-    def withdraw(self, number):
-        self.total = self.total - number
-
-
-
 class Agency(Group):
 
     def solo_debut(self):
@@ -51,3 +65,14 @@ class Agency(Group):
 
     def group_debut(self):
         pass
+
+if __name__ == "__main__":
+
+    jay = People('제이', '재프')
+    jay.join(10)
+    jay.performance()
+    print(jay.fanclub)
+    print(jay.total)
+    jay.performance()
+    print(jay.total)
+    print(jay.perform)
