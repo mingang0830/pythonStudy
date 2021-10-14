582e29aa88bada77c3292baf38140f2c2acdd842
mingyeong <mingang0830@gmail.com>
2021-10-14 18:06:37 +0900

    아이돌 서비스
    앨범을 내면 팬클럽 회원수가 가장 많은 팬덤은 70%, 두번째 팬덤은 60%, 세번째 팬덤은 50%, 나머지는 30%의 확률로 성공합니다.
    앨범 발매에 성공하면 팬클럽 회원수가 30% 증가하고, 실패하면 10% 감소합니다.

--- idol_system.py
+++ idol_system.py
@@ -3,7 +3,7 @@ from random import choice
 
 class Fanclub:
     def __init__(self, fanclub):
-        self.total = 0  # 팬클럽 회원 수
+        self.total = 10  # 팬클럽 회원 수
         self.fanclub = fanclub  # 팬클럽 이름
 
     def join(self, number):
@@ -34,23 +34,61 @@ class People(Fanclub):
             self.total += int(self.total * 0.1)
         else:
             # 30% 확률로 팬클럽 생성
-            if choice([1,1,1,2,2,2,2,2,2]) == 1:
+            if choice([1, 1, 1, 2, 2, 2, 2, 2, 2]) == 1:
                 self.fanclub = input('팬클럽 생성 : ')  # 팬클럽 이름 지정
                 self.total = 10  # 팬클럽 회원수 추가
 
     def audition(self):
         pass
 
-    def release_album(self):
-        pass
+    def release_album(self, f_lst):  # 딕셔너리 받기
+
+        #딕셔너리 값을 기준으로 내림차준 정렬
+        sort_f = sorted(f_lst.values())
+
+        # 1,2,3위 변수 지정
+        first = sort_f[0]
+        second = sort_f[1]
+        third = sort_f[2]
+
+        if self.total == first:
+            if choice([1, 1, 1, 1, 1, 1, 1, 2, 2, 2]) == 1:  # 1위가 성공하면
+                self.total += int(self.total * 0.3)
+                print('성공')
+            else:  # 1위가 실패하면
+                self.total -= int(self.total * 0.1)
+                print('실페')
+
+        elif self.total == second:
+            if choice([1, 1, 1, 2, 2]) == 1:  # 2위가 성공하면
+                self.total += int(self.total * 0.3)
+                print('성공')
+            else:  # 2위가 실패하면
+                self.total -= int(self.total * 0.1)
+                print('실페')
+
+        elif self.total == third:
+            if choice([1, 2]) == 1:  # 3위가 성공하면
+                self.total += int(self.total * 0.3)
+                print('성공')
+            else:  # 3위가 실패하면
+                self.total -= int(self.total * 0.1)
+                print('실페')
+
+        else:  # 나머지 등수
+            if choice([1, 1, 1, 2, 2, 2, 2, 2, 2, 2]) == 1:
+                self.total += int(self.total * 0.3)
+                return '성공'
+            else:
+                self.total -= int(self.total * 0.1)
+                return '실패'
 
 
 class Idol(People):  # 솔로
     def __init__(self, i_name, g_name, fanclub, name):
-        super().__init__(name)
+        super().__init__(name, fanclub)
         self.i_name = i_name  # 아이돌 이름
         self.g_name = g_name  # 그룹 이름
-        self.fanclub = fanclub  # 팬클럽 이름
 
 
 class Group(Idol):  # idol에서 데려오기
@@ -66,13 +104,30 @@ class Agency(Group):
     def group_debut(self):
         pass
 
+
 if __name__ == "__main__":
 
     jay = People('제이', '재프')
-    jay.join(10)
     jay.performance()
     print(jay.fanclub)
-    print(jay.total)
+
     jay.performance()
-    print(jay.total)
-    print(jay.perform)
+    jaf = jay.total
+
+    min = People('민','민트')
+    min.performance()
+    print(min.fanclub)
+    min.join(50)
+    mint = min.total
+
+    qq = People('큐', '큐프')
+    qq.performance()
+    qq.join(2)
+    qf = qq.total
+
+    f_lst = {'mint': mint, 'jaf': jaf, 'qf': qf}
+
+    print(f_lst)
+
+    min.release_album(f_lst)
+    print(min.total)
