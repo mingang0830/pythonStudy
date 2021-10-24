6657e090767ae4bcdcd432986438616655ab204f
mingyeong <mingang0830@gmail.com>
2021-10-24 21:17:08 +0900

    학점관리시스템
    subject 데이터와 user로 파일을 분리
    
    score class에 static 변수로 점수 저장.
    subject class를 상속받는 pass타입과 score타입으로 분류함.
    subject class에 필수과목 만드는 메소드 추가(make_essential)
    pass타입과 score타입에서는 사용 못하게 오버라이딩함.
    
    pro class에 교수가 필수과목을 본인이 할당받는 메소드 추가(assign_essential)
    student class에서 본인이 수강한 과목의 점수를 확인할 수 있는 메소드 추가(my_score)

--- student_system.py
+++ student_system.py
@@ -1,55 +1,32 @@
-class Student:
-    def __init__(self, name):
-        self.name = name
-        self.subject_lst = []
+from student_system_data import Subject, SubjectScore, SubjectPass, Score
+from student_system_user import Pro, Student
 
 
-class Subject:
-    def __init__(self, name, desc, pro, limit):
-        self.name = name
-        self.desc = desc
-        self.pro = pro
-        self.limit = limit
-        self.sub_stu = []
-        self.is_deleted = False
+def program():
 
-    def join(self, student):
-        num = len(self.sub_stu)
-        if self.is_deleted is False and num < self.limit:
-            self.sub_stu.append(student)
-            student.subject_lst.append(self)
+    essential_subjects = Subject.make_essential()
 
-        else:
-            return False
-
-    def close(self):
-        if len(self.sub_stu) == 0:
-            self.is_deleted = True
-            self.sub_stu = []
-
-
-class Pro:
-    def __init__(self):
-        self.subject = []
-
-    def open_sub(self, name, desc, limit):
-        my_sub = Subject(name, desc, self, limit)
-        self.subject.append(my_sub)
-        return my_sub
-
-
-if __name__ == "__main__":
-    p1 = Pro()
-    p2 = Pro()
+    p1 = Pro('김교수')
+    p2 = Pro('이교수')
     stu1 = Student("김동욱")
 
     sub_list = []  # 개설된 강의 리스트
-    for pro, name, desc, limit in [(p1, "자료구조", "자료의 구조", 1),
-                                   (p1, "컴퓨터의 기본", "기본", 2),
-                                   (p2, "수학개론", "수학", 3),
-                                   (p2, "물리개론", '물리', 3),
-                                   (p2, "생물개론", '생물', 5)]:
-        sub_list.append(pro.open_sub(name, desc, limit))
+
+    for pro, name, desc, limit, type_ in [(p1, "자료구조", "자료의 구조", 1, SubjectScore),
+                                   (p1, "컴퓨터의 기본", "기본", 2, SubjectPass),
+                                   (p2, "수학개론", "수학", 3, SubjectScore),
+                                   (p2, "물리개론", '물리', 3, SubjectScore),
+                                   (p2, "생물개론", '생물', 5, SubjectScore)]:
+        sub_list.append(pro.open_sub(name, desc, limit, type_))
+
+    return sub_list, stu1, p1, essential_subjects
+
+
+if __name__ == "__main__":
+
+    sub_list, stu1, p1, essential_subjects = program()
+
+    p1.assign_essential(essential_subjects)  # side effect
 
     sub_list[0].join(stu1)  # 수강신청
     sub_list[2].join(stu1)
@@ -57,4 +34,17 @@ if __name__ == "__main__":
 
     for sub in sub_list:  # 정원 안 찬 강의 폐강시키기
         sub.close()
-        print(sub.is_deleted)
+
+    #sub_list[0].check(stu1, Score.A)
+    sub_list[1].check(stu1, Score.PASS)
+    #sub_list[4].check(stu1, Score.A)
+    for i in p1.subject:
+        print(i)
+        print(i.name)
+
+
+
+
+
+
+
