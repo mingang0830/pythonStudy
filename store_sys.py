7084705a3c1fead57fd6275c6f8b732415f5a037
mingyeong <mingang0830@gmail.com>
2021-11-03 19:03:16 +0900

    store 시스템에 db 붙이기
    Store.check_stock을 db를 통해서 만드는게 아니라 찝찝함. 또 데이터 타입을 맞추려고 object를 키로 넣는게 아니라 튜플로 만들어서 키에 넣었는데 이게 맞을지..
    
    stock db에 id 를 primary key로 지정하고 autoincrement 설정 했는데 왜 1부터 안들어가고 5부터 들어가는지 모르겠음

--- store_sys.py
+++ store_sys.py
@@ -5,30 +5,38 @@ cur = conn.cursor()
 
 
 class Product:
-    def __init__(self, id, name, price, store):
-        self.id = id
-        self.name = name
-        self.price = price
-        self.store = store
+    def __init__(self, id, name, price):
+        cur.execute("insert into product values(?,?,?)", (id, name, price))
+        self.id = cur.execute("select id from product where id={}".format(id)).fetchone()[0]
+        self.name = cur.execute("select name from product where id={}".format(id)).fetchone()[0]
+        self.price = cur.execute("select price from product where id={}".format(id)).fetchone()[0]
 
     def __repr__(self):
-        return f'({self.name} - {self.price}원 )'
+        return f"('{self.name}', {self.price})"
 
 
 class Store:
-    def __init__(self, store_name):
-        self.store_name = store_name[1]
-        self.products = {}
+    def __init__(self, name):
+        cur.execute("insert into store (name) values('{}')".format(name))
+        self.id = cur.execute("select id from store where name='{}'".format(name)).fetchone()[0]
+        self.name = cur.execute("select name from store where name='{}'".format(name)).fetchone()[0]
         self.check_stock = {}
 
     def buy(self, product, amount):
-        self.products[product.id] = product
-        self.check_stock[product] = amount
+        cur.execute("insert into stock (store_id, product_id) values(?,?)", (self.id, product.id))
+        self.check_stock[(product.name, product.price)] = amount
+
+    def products(self):
+        products = {}
+        for product_id in cur.execute("select product_id from stock where store_id={}".format(self.id)).fetchall():
+            if product_id == cur.execute("select id from product where id='{}'".format(product_id[0])).fetchone():
+                products[product_id[0]] = cur.execute("select name, price from product where id={}".format(product_id[0])).fetchone()
+        return products
 
     def get_product_id_by_name(self, name):
-        for id in self.products:
-            if self.products[id].name == name:
-                return id
+        for product_id in cur.execute("select product_id from stock where store_id={}".format(self.id)).fetchall():
+            if product_id == cur.execute("select id from product where name='{}'".format(name)).fetchone():
+                return product_id[0]
 
     def out_of_stock(self):
         out_of_stock_items = []
@@ -38,8 +46,7 @@ class Store:
         return out_of_stock_items
 
     def __repr__(self):
-        return self.store_name
-
+        return f"{self.name}"
 
 class Customer:
     def __init__(self):
@@ -47,50 +54,51 @@ class Customer:
 
     def buy(self, store, name, amount):
         product_id = store.get_product_id_by_name(name)
-        product = store.products[product_id]
+        product = store.products()[product_id]
         new_amount = store.check_stock[product] - amount
         if new_amount >= 0:
             store.check_stock[product] = new_amount
             self.purchase_list.append({'store': store, 'product': product, 'amount': amount})
-            print(f'{product.name} - {amount}개 구매완료!')
+            print(f'{product} - {amount}개 구매완료!')
         else:
             raise Exception('상품의 재고가 부족합니다.')
 
 
 if __name__ == "__main__":
 
-    stationery_store = cur.execute("select * from store where id=1").fetchone()
-    s1 = Store(stationery_store)
+    s1 = Store('문방구')
+    s2 = Store('문방구2')
     c1 = Customer()
+    # print(s1.id)
+    # print(s1.name)
 
-    # product에 물품 추가
-    cur.execute("INSERT INTO product Values(5, '마카', 2000);")
-    cur.execute("INSERT INTO stock Values(5, 1, 5);")
+    for id, name, price, amount in [(1, '연필', 1000, 4),
+                                    (2, '샤프', 1000, 3),
+                                    (3, '지우개', 1000, 2),
+                                    (4, '화이트', 1000, 1),
+                                    (5, '형광펜', 1600, 4)]:
+        s1.buy(Product(id, name, price), amount)
 
-    # product에서 물건 가져오기
-    pencil = cur.execute("select * from product where id=1").fetchone()
-    sharp = cur.execute("select * from product where id=2").fetchone()
-    eraser = cur.execute("select * from product where id=3").fetchone()
-    white = cur.execute("select * from product where id=4").fetchone()
-    mark = cur.execute("select * from product where id=5").fetchone()
+    tape = Product(6, '테이프', 1200)
+    paper = Product(7, '종이', 1100)
 
-    for id, name, price in [pencil,
-                            sharp,
-                            eraser,
-                            white,
-                            mark]:
-        s1.buy(Product(id, name, price, s1), 4)
+    s1.buy(tape, 2)
+    s2.buy(tape, 3)
+    s2.buy(paper, 2)
 
-    print(s1.products)
-    print(s1.get_product_id_by_name('마카'))
+    print(s1.products())
+    # print(s2.products())
+
+    # print(s2.get_product_id_by_name('종이'))
+    #
+    print(cur.execute("select * from stock").fetchall())  # 왜 stock의 id가 1부터 안들어가지?
 
     c1.buy(s1, '연필', 4)
-    c1.buy(s1, '샤프', 3)
-    c1.buy(s1, '지우개', 2)
-
     print(c1.purchase_list)
+
     print(s1.check_stock)
     print(s1.out_of_stock())
 
-    # print([row for row in cur.execute("select * from product")])
+
+
 
