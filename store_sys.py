2bc446379f28fc8e29e32d02e7fbdb28fe024dd9
mingyeong <mingang0830@gmail.com>
2021-11-04 13:33:58 +0900

    store 시스템에 db 붙이기

--- store_sys.py
+++ store_sys.py
@@ -7,31 +7,26 @@ cur = conn.cursor()
 class Product:
     def __init__(self, id, name, price):
         cur.execute("insert into product values(?,?,?)", (id, name, price))
-        self.id = cur.execute("select id from product where id={}".format(id)).fetchone()[0]
-        self.name = cur.execute("select name from product where id={}".format(id)).fetchone()[0]
-        self.price = cur.execute("select price from product where id={}".format(id)).fetchone()[0]
+        self.id = id
+        self.name = name
+        self.price = price
 
     def __repr__(self):
         return f"('{self.name}', {self.price})"
 
 
 class Store:
-    def __init__(self, name):
-        cur.execute("insert into store (name) values('{}')".format(name))
-        self.id = cur.execute("select id from store where name='{}'".format(name)).fetchone()[0]
-        self.name = cur.execute("select name from store where name='{}'".format(name)).fetchone()[0]
+    def __init__(self, id, name):
+        cur.execute("insert into store values({}, '{}')".format(id, name))
+        self.id = id
+        self.name = name
         self.check_stock = {}
+        self.products = {}
 
     def buy(self, product, amount):
         cur.execute("insert into stock (store_id, product_id) values(?,?)", (self.id, product.id))
-        self.check_stock[(product.name, product.price)] = amount
-
-    def products(self):
-        products = {}
-        for product_id in cur.execute("select product_id from stock where store_id={}".format(self.id)).fetchall():
-            if product_id == cur.execute("select id from product where id='{}'".format(product_id[0])).fetchone():
-                products[product_id[0]] = cur.execute("select name, price from product where id={}".format(product_id[0])).fetchone()
-        return products
+        self.check_stock[product] = amount
+        self.products[product.id] = product
 
     def get_product_id_by_name(self, name):
         for product_id in cur.execute("select product_id from stock where store_id={}".format(self.id)).fetchall():
@@ -48,14 +43,16 @@ class Store:
     def __repr__(self):
         return f"{self.name}"
 
+
 class Customer:
     def __init__(self):
         self.purchase_list = []
 
     def buy(self, store, name, amount):
         product_id = store.get_product_id_by_name(name)
-        product = store.products()[product_id]
+        product = store.products[product_id]
         new_amount = store.check_stock[product] - amount
+
         if new_amount >= 0:
             store.check_stock[product] = new_amount
             self.purchase_list.append({'store': store, 'product': product, 'amount': amount})
@@ -66,8 +63,8 @@ class Customer:
 
 if __name__ == "__main__":
 
-    s1 = Store('문방구')
-    s2 = Store('문방구2')
+    s1 = Store(1, '문방구')
+    s2 = Store(2, '문방구2')
     c1 = Customer()
     # print(s1.id)
     # print(s1.name)
@@ -86,7 +83,7 @@ if __name__ == "__main__":
     s2.buy(tape, 3)
     s2.buy(paper, 2)
 
-    print(s1.products())
+    print(s1.products)
     # print(s2.products())
 
     # print(s2.get_product_id_by_name('종이'))
