bb4f33b9ba1e9f97795fa28b25ff023d271f082e
mingyeong <mingang0830@gmail.com>
2022-01-19 09:49:38 +0900

    2018 day3
    가독성 좋게 수정
    정규표현식 사용

--- 2018_day3.py
+++ 2018_day3.py
@@ -1,54 +1,77 @@
+import re
+from collections import defaultdict
+
 file_path = "2018_day3.txt"
 with open(file_path) as f:
     puzzle = f.read().splitlines()
 
 
 def parse(puzzle):
+    """
+    docstring == document string
+    함수의 설명
+    parse 는 puzzle 을 입력받아
+    id,x,y,wide,tall 을 키로 하는 dict를 반환합니다.
+
+    example: 입력받는 puzzle의 예제
+    #1 @ 387,801: 11x22
+    ...
+    """
     claims = []
-    for data in puzzle:
-        id = int(data.split(" ")[0][1:])
-        x, y = int(data.split(" ")[2].split(",")[0]), int(data.split(" ")[2].split(",")[1][:-1])
-        wide, tall = int(data.split(" ")[3].split("x")[0]), int(data.split(" ")[3].split("x")[1])
-        claims.append({"id": id, "x": x, "y": y, "wide": wide, "tall": tall})
+    for line in puzzle:
+        id_, x, y, width, height = map(int, re.findall(r"^#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)$", line)[0])
+        claims.append({
+            "id": id_,
+            "x": x,
+            "y": y,
+            "wide": width,
+            "tall": height})
     return claims
 
 
 claims = parse(puzzle)
 
+
+def make_square(x, y, width, height):
+    result = []
+    for w in range(width):
+        for h in range(height):
+            result.append((x + w, y + h))
+    return result
+
+
 id_and_inches = {}
-inches_of_id = []
-all_inches = []
 for claim in claims:
-    for w in range(claim["wide"]):
-        for t in range(claim["tall"]):
-            all_inches.append((claim["x"] + w, claim["y"] + t))
-            inches_of_id.append((claim["x"] + w, claim["y"] + t))
-    id_and_inches[claim["id"]] = inches_of_id
-    inches_of_id = []
-
-result = {}
-
-for inch in all_inches:
-    if inch not in result:
-        result[inch] = 1
-    else:
-        result[inch] += 1
-
-count = 0
-not_overlap = []
-for k, v in result.items():
-    if v >= 2:
-        count += 1
-    elif v == 1:
-        not_overlap.append(k)
-
-print("part1 : {}".format(count))
-
-set_not_overlap = set(not_overlap)
-
-for k, v in id_and_inches.items():
-    set_v = set(v)
-    if set_v - set_not_overlap == set():
-        print("part2 : {}".format(k))
+    id_and_inches[claim["id"]] = make_square(claim["x"], claim["y"], claim["wide"], claim["tall"])
+    # id_and_inches = {1: [(0, 0), (0, 1), (1, 0), (1, 1)], 2: ..., ...}
 
 
+def inch_count(data):
+    result = defaultdict(int)
+    for coords in data:
+        for coord in coords:
+            result[coord] += 1
+    return result
+
+
+result = inch_count(id_and_inches.values())
+# result = {(0,0): 1, (0,1): 1, ... (3,3): 3, ...}
+
+part1_answer = 0
+for coord, count in result.items():  # 1차원의 고정된 loop == O(n)
+    if count > 1:
+        part1_answer += 1
+
+not_overlapped = set()
+for coord, count in result.items():  # 1차원의 고정된 loop == O(n)
+    if count == 1:
+        not_overlapped.add(coord)
+# O(n) + O(n) != 2O(n)
+# O(n) + O(n) = O(n)
+# 가독성을 챙기는게 낫다.
+
+print("part1 : {}".format(part1_answer))
+
+for id_, coords in id_and_inches.items():
+    if not set(coords) - not_overlapped:
+        print("part2 : {}".format(id_))
